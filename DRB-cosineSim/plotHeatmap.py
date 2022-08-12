afe96eee4ed1638051751a223a1915423193acab
Pei-Hung Lin <lin32@llnl.gov>
2022-08-12 00:50:44 -0700

    cosine sim. heatmaps

---
+++ DRB-cosineSim/plotHeatmap.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def convert(l):
+    df2 = pd.DataFrame(index=range(625),columns=range(625),dtype=np.float64)
+    df2 = df2.iloc[1:]
+    idx = 0
+    for i in range(1, 625):
+        for n in range(1,625):
+#            print(i,n,l[idx])
+            df2[n][i] = l[idx]
+            idx = idx + 1
+    return df2
+
+csv1="cosine_similarity_distanceBase.csv"
+csv2="cosine_similarity_codeBERT.csv"
+csv3="cosine_similarity_POJ.csv"
+csv4="cosine_similarity_DRB.csv"
+
+# model 1: distanceBased
+d1 = pd.read_csv(csv1,float_precision='round_trip',header=None)
+d1 = d1.apply(pd.to_numeric, errors='coerce')
+d1 = d1.iloc[1:]
+d1 = d1.drop(d1.columns[[0]], axis=1)
+d1.index.names = ['Code Region']
+
+#model 2: vanilla bert
+d2 = pd.read_csv(csv2,float_precision='round_trip',header=None)
+d2 = d2.iloc[:,2]
+
+df2 = convert(d2) 
+#print(df2)
+
+#model 3: POJ bert
+d3 = pd.read_csv(csv3,float_precision='round_trip',header=None)
+d3 = d3.iloc[:,2]
+
+df3 = convert(d3) 
+#print(df3)
+
+#model 4: DRB bert
+d4 = pd.read_csv(csv3,float_precision='round_trip',header=None)
+d4 = d4.iloc[:,2]
+
+df4 = convert(d4) 
+#print(df4)
+
+plt.rcParams["figure.figsize"] = [16.0, 4.00]
+plt.rcParams["figure.autolayout"] = True
+
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(ncols=4)
+fig.subplots_adjust(wspace=0.01)
+
+#sns.heatmap( d1, cmap="rocket_r",ax=ax1)
+#sns.heatmap(df2, cmap="rocket_r",ax=ax2)
+#sns.heatmap(df3, cmap="rocket_r",ax=ax3)
+#sns.heatmap(df4, cmap="rocket_r",ax=ax4)
+
+sns.heatmap( d1, ax=ax1,  vmin= 0., vmax=1.0, cbar=False)
+sns.heatmap(df2, ax=ax2,  vmin= 0., vmax=1.0, cbar=False)
+sns.heatmap(df3, ax=ax3,  vmin= 0., vmax=1.0, cbar=False)
+sns.heatmap(df4, ax=ax4,  vmin= 0., vmax=1.0, cbar=True)
+fig.subplots_adjust(wspace=0.001)
+plt.savefig('heatmap.png')
+plt.show()
+##    data = pd.read_csv(csv_path, header=None)
+#    data = pd.read_csv(csv_path,float_precision='round_trip',header=None)
+#    data = data.iloc[:,2]
+#    df2 = pd.DataFrame(index=range(625),columns=range(625),dtype=np.float64)
+#    df2 = df2.iloc[1:]
+#    #df2.index = df2.index + 1
+#
+#
+#    idx = 0
+#
+#    for i in range(1, 625):
+#        for n in range(1,625):
+##            print(i,n,data[idx])
+#            df2[n][i] = data[idx]
+#            idx = idx + 1
+#
+#    print(df2)
+##    data = data.drop(data.columns[[0]], axis=1)
+##    print(data)
+##    df2.index.names = ['Code Region']
+#    g = sns.heatmap(df2,cmap="rocket_r")
+##    g = sns.heatmap(data,cmap="PiYG",annot=True, vmin = 0.7, vmax = 1.0)
+#    g.set_yticklabels(g.get_yticklabels(), rotation=0)
+#    g.set_title('Heatmap')
+##    plt.tight_layout()
+##    plt.show()
+#    g.figure.savefig("heatmap.png")
+#
+#
+#extract_csv_gen_plot("cosine_similarity_DRB.csv")
